fee554ed856009d7e5ec1346ac794946e49a6a79
krish <praneethbellamkonda@gmail.com>
2018-04-11 17:05:14 -0400

    Added code for rodent classification

--- Rodent-Activity-Classification/transfer_learning_vgg16_custom_data.py
+++ Rodent-Activity-Classification/transfer_learning_vgg16_custom_data.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import os
 import time
@@ -13,28 +12,23 @@ from keras.utils import np_utils
 from sklearn.utils import shuffle
 from sklearn.cross_validation import train_test_split
 
-img_path = 'c57.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-print (x.shape)
-x = np.expand_dims(x, axis=0)
-print (x.shape)
-x = preprocess_input(x)
-print('Input image shape:', x.shape)
 
 # Loading the training data
 PATH = os.getcwd()
 # Define data path
 data_path = PATH + '/data'
 data_dir_list = os.listdir(data_path)
+data_dir_list.remove('.DS_Store')
+
+data_dir_list = ['eat', 'hang', 'rear', 'drink']
 
 img_data_list=[]
 
 for dataset in data_dir_list:
-	img_list=os.listdir(data_path+'/'+ dataset)
+	img_list=glob.glob(os.path.join(data_path+'/'+ dataset,'*jpg'))
 	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
 	for img in img_list:
-		img_path = data_path + '/'+ dataset + '/'+ img
+		img_path = img
 		img = image.load_img(img_path, target_size=(224, 224))
 		x = image.img_to_array(img)
 		x = np.expand_dims(x, axis=0)
@@ -57,12 +51,20 @@ num_classes = 4
 num_of_samples = img_data.shape[0]
 labels = np.ones((num_of_samples,),dtype='int64')
 
-labels[0:202]=0
-labels[202:404]=1
-labels[404:606]=2
-labels[606:]=3
 
-names = ['cats','dogs','horses','humans']
+
+len_eat = len(glob.glob(os.path.join(data_path+'/'+'eat','*.jpg')))
+len_hang = len(glob.glob(os.path.join(data_path+'/'+'hang','*.jpg')))
+len_rear = len(glob.glob(os.path.join(data_path+'/'+'rear','*.jpg')))
+len_drink = len(glob.glob(os.path.join(data_path+'/'+'drink','*.jpg')))
+
+labels[0:len_eat]=0
+labels[len_eat:len_eat+len_hang]=1
+labels[len_eat+len_hang:len_eat+len_hang+len_rear]=2
+labels[len_eat+len_hang+len_rear:len_eat+len_hang+len_rear+len_drink]=3
+
+
+names = ['eat', 'hang', 'rear', 'drink']
 
 # convert class labels to on-hot encoding
 Y = np_utils.to_categorical(labels, num_classes)
@@ -88,7 +90,7 @@ custom_vgg_model.summary()
 for layer in custom_vgg_model.layers[:-1]:
 	layer.trainable = False
 
-custom_vgg_model.layers[3].trainable
+#custom_vgg_model.layers[3].trainable
 
 custom_vgg_model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
 
@@ -104,38 +106,14 @@ print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
 
 ####################################################################################################################
 
-#Training the feature extraction also
-
-image_input = Input(shape=(224, 224, 3))
-
-model = VGG16(input_tensor=image_input, include_top=True,weights='imagenet')
-
-model.summary()
-
-last_layer = model.get_layer('block5_pool').output
-x= Flatten(name='flatten')(last_layer)
-x = Dense(128, activation='relu', name='fc1')(x)
-x = Dense(128, activation='relu', name='fc2')(x)
-out = Dense(num_classes, activation='softmax', name='output')(x)
-custom_vgg_model2 = Model(image_input, out)
-custom_vgg_model2.summary()
-
-# freeze all the layers except the dense layers
-for layer in custom_vgg_model2.layers[:-3]:
-	layer.trainable = False
-
-custom_vgg_model2.summary()
-
-custom_vgg_model2.compile(loss='categorical_crossentropy',optimizer='adadelta',metrics=['accuracy'])
-
-t=time.time()
-#	t = now()
-hist = custom_vgg_model2.fit(X_train, y_train, batch_size=32, epochs=12, verbose=1, validation_data=(X_test, y_test))
-print('Training time: %s' % (t - time.time()))
-(loss, accuracy) = custom_vgg_model2.evaluate(X_test, y_test, batch_size=10, verbose=1)
-
-print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
-
+# serialize model to JSON
+custom_vgg_model_json = custom_vgg_model.to_json()
+with open("custom_vgg_model_json", "w") as json_file:
+    json_file.write(custom_vgg_model_json)
+# serialize weights to HDF5
+model.save_weights("custom_vgg_model_json.h5")
+print("Saved model to disk")
+ 
 
 import matplotlib.pyplot as plt
 # visualizing losses and accuracy

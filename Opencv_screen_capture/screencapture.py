f1e081fb579f42323f44aabd8fcc51d01a6bbbf1
Krish <krish201692@gmail.com>
2018-04-18 10:36:02 -0400

    Added code for screencapture

--- Opencv_screen_capture/screencapture.py
+++ Opencv_screen_capture/screencapture.py
@@ -9,9 +9,12 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 
-img = ImageGrab.grab(bbox=(100,100,400,400))
-img_np = np.array(img)
-frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-cv2.imshow("frame",frame)
-cv2.waitKey(0)
+while(True):
+    img = ImageGrab.grab(bbox=(100,10,600,500))
+    img_np = np.array(img)
+    cv2.imshow("frame",img_np)
+    key= cv2.waitKey(1)
+    if key == 'q':
+        break
+#vid.release()
 cv2.destroyAllWindows()

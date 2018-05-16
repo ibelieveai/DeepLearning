8b1d18a203b5b44cc848f198e9f44589c6e1e960
krish <praneethbellamkonda@gmail.com>
2018-05-16 17:29:32 -0400

    implemented the detect function

--- Object-Detection-SSD/object_detection.py
+++ Object-Detection-SSD/object_detection.py
@@ -14,3 +14,38 @@ from data import BaseTransform , VOC_CLASSES as labelmap #Image transformation a
 from ssd import build_ssd #build helps in construct the single shot multibox detection model
 import imageio #process the images
 
+
+#defining the detect function
+def detect(frame, net, transform):
+    height, width = frame.shape[:2]
+    #Transformation of the frame which results a numpy array
+    frame_t = transform(frame)[0]
+    #Convert numpy array to torch tensor (Advanced matirx ) and convert RGB to GRB
+    x = torch.from_numpy(frame_t).permute(2,0,1)
+    #adding fake dimension for batch size as neural network only accepts the images in batches.
+    x = Variable(x.unsqueeze(0))
+    y = net(x) #passing the torch variable to neural network
+    detections = y.data #extracting data
+    scale = torch.Tensor([width,height,width,height])
+    #detections = [batches, number of classes, number of occurances,tuple of {score,x0,y0,x1,y1}]
+    for i in range(detections.size(1)):
+        j = 0 #occurance of the class
+        while detection[0,i,j,0]>0.6:#score for occurance j of class i
+            pt = (detections[0,i,j,1:] * scale).numpy()
+            # imagge, co-ordinates of upper left , co-ordinates of lower right, color of rectangle , thickness of text to display
+            cv2.rectangle(frame,(int(pt[0]),int(pt[1])),(int(pt[2]),int(pt[3])),(255,0,0),2) 
+            # image, name to print , where to print, font , color, thickness of text, continues text
+            cv2.putText(frame,labelmap[i - 1], (int(pt[0]),int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255),2,cv2.LINE_AA)
+            
+            
+            
+            
+            
+        
+    
+    
+    
+    
+    
+    
+    

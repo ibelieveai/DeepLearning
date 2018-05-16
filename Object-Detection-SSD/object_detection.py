7911964ce0fe4afde911704c8063760eade7216d
krish <praneethbellamkonda@gmail.com>
2018-05-16 16:43:44 -0400

    Added required libraries

--- Object-Detection-SSD/object_detection.py
+++ Object-Detection-SSD/object_detection.py
@@ -6,3 +6,11 @@ Created on Wed May 16 15:41:09 2018
 @author: krish
 """
 
+# Importing required packages for object detection.
+import torch # deeplearning framework as dynamic graphs helps us in faster computations
+from torch.autograd import Variable #adagrad module responsible for gradient desent..variable class helps in converting tensors to torch variable 
+import cv2 #Used to draw rectangles on top of the image
+from data import BaseTransform , VOC_CLASSES as labelmap #Image transformation and class labels
+from ssd import build_ssd #build helps in construct the single shot multibox detection model
+import imageio #process the images
+
